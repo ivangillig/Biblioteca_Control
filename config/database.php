88d28bcf2c4039d4f782b7189467ccfd8735bbfa
Ivan Gillig <48303606+ivangillig@users.noreply.github.com>
2022-06-04 21:28:32 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,6 +1,11 @@
 <?php
+
+//------------------------------------------------------------------------------------
+TRABAJO PRACTICO PARA LA FACULTAD, EVITA MODIFICAR LA BASE DE DATOS, APP DE PRUEBA
+//------------------------------------------------------------------------------------
+
 function conectarDB() : mysqli {
-    $db = mysqli_connect('localhost', 'root', '', 'biblioteca');
+    $db = mysqli_connect('us-cdbr-east-05.cleardb.net', 'bf0d4e6b52a0ac', '5a4d20c3', 'heroku_882f3b2d978bb47');
     
     if(!$db){
         echo 'Error no se pudo conectar';
