8e83ae09b14389a93a21026c2d8613366f16c1cf
Ivan Gillig <48303606+ivangillig@users.noreply.github.com>
2022-06-04 22:55:03 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -2,8 +2,15 @@
 
 
 function conectarDB() : mysqli {
-    $db = mysqli_connect('localhost', 'root', '', 'biblioteca');
+    //$db = mysqli_connect('localhost', 'root', '', 'biblioteca');
     
+    $db = mysqli_connect (
+            $_ENV['DB_HOST'],
+            $_ENV['DB_USER'],
+            $_ENV['DB_PASS'],
+            $_ENV['DB_BD']
+            );
+
     if(!$db){
         echo 'Error no se pudo conectar';
         exit;
